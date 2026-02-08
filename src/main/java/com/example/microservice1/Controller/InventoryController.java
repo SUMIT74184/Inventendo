df01c1fe39cbad744bc8c21ce970bd19e96ca8c0
SUMIT <rais74184@gmail.com>
2026-02-08 12:32:16 +0530

    Inventory-controller-services-repository-DTO

--- src/main/java/com/example/microservice1/Controller/InventoryController.java
+++ src/main/java/com/example/microservice1/Controller/InventoryController.java
@@ -1,16 +1,96 @@
 package com.example.microservice1.Controller;
 
 
+import com.example.microservice1.Dto.InventoryRequest;
+import com.example.microservice1.Dto.InventoryResponse;
+import com.example.microservice1.Service.InventoryService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
+@RequestMapping("api/v1/inventory")
+@RequiredArgsConstructor
 @ResponseStatus(HttpStatus.OK)
 public class InventoryController {
-    @GetMapping("/")
-    public boolean IsStock(){
-    return true;
+
+    private final InventoryService inventoryService;
+
+    @PostMapping
+    public ResponseEntity<InventoryResponse> createInventory(@Valid @RequestBody InventoryRequest request){
+        InventoryResponse response = inventoryService.createInventory(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
+
+    @GetMapping("/{sku}")
+    public ResponseEntity<InventoryResponse> getInventoryBySku(@PathVariable String sku){
+        InventoryResponse response = inventoryService.getInventoryBySku(sku);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<InventoryResponse>> getAllInventory(){
+        List<InventoryResponse> response = inventoryService.getAllInventory();
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/warehouse/{warehouseId")
+    public ResponseEntity<List<InventoryResponse>> getInventoryByWareHouse(@PathVariable String warehouseId){
+        List<InventoryResponse>response = inventoryService.getInventoryByWarehouse(warehouseId);
+        return  ResponseEntity.ok(response);
+
+    }
+    @GetMapping("/low-stock")
+    public ResponseEntity<List<InventoryResponse>> getLowStocksItems(){
+        List<InventoryResponse>response = inventoryService.getLowStockItems();
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping("/{sku}/quantity")
+    public ResponseEntity<InventoryResponse> updateQuantity(
+            @PathVariable String sku,
+            @RequestBody Map<String,Integer> request
+    ){
+        InventoryResponse response = inventoryService.updateQuantity(sku,request.get("quantity"));
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/{sku}/check")
+    public ResponseEntity<Map<String,Boolean>> checkAvailability(
+            @PathVariable String sku,
+            @RequestParam Integer quantity
+    ){
+        boolean available = inventoryService.checkAvailability(sku,quantity);
+        return ResponseEntity.ok(Map.of("available",available));
+    }
+
+    @PostMapping("/{sku}/reserve")
+    public ResponseEntity<Void> reserveStock(
+            @PathVariable String sku,
+            @RequestBody Map<String,Integer> request
+    ){
+        inventoryService.reserveStock(sku,request.get("quantity"));
+        return ResponseEntity.ok().build();
+    }
+
+    public ResponseEntity<Void> releaseStock(
+            @PathVariable String sku,
+            @RequestBody Map<String,Integer> request
+    ){
+        inventoryService.releaseReservedStock(sku,request.get("quantity"));
+        return ResponseEntity.ok().build();
+    }
+
+
+
+
+
+
 }
---
+++ src/main/java/com/example/microservice1/Service/InventoryService.java
@@ -0,0 +1,130 @@
+package com.example.microservice1.Service;
+
+import com.example.microservice1.Dto.InventoryRequest;
+import com.example.microservice1.Dto.InventoryResponse;
+import com.example.microservice1.Exception.InsufficientStockException;
+import com.example.microservice1.Exception.InventoryNotFoundException;
+import com.example.microservice1.Repository.InventoryRepository;
+import com.example.microservice1.model.Inventory;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class InventoryService {
+
+    private final InventoryRepository inventoryRepository;
+    private final KafkaTemplate<String,Object> KafkaTemplate;
+
+    @Transactional
+    @CacheEvict(value = "inventory", key = "#request.sku")
+    public InventoryResponse createInventory(InventoryRequest request){
+        Inventory inventory = new Inventory();
+        inventory.setSku(request.getSku());
+        inventory.setDescription(request.getDescription());
+        inventory.setQuantity(request.getQuantity());
+        inventory.setReorderLevel(request.getReorderLevel());
+        inventory.setMaxStockLevel(request.getMaxStockLevel());
+        inventory.setUnitPrice(request.getUnitPrice());
+        inventory.setWarehouseId(request.getWarehouseId());
+        inventory.setLocation(request.getLocation());
+
+        Inventory saved = inventoryRepository.save(inventory);
+        log.info("Created inventory for SKU: {}",saved.getSku());
+
+        //sending the data to the kafka
+        KafkaTemplate.send("inventory-created",saved.getSku(),saved);
+        return InventoryResponse.fromEntity(saved);
+
+    }
+
+
+    @Cacheable(value = "inventory", key = "#sku")
+    public InventoryResponse getInventoryBySku(String sku){
+        Inventory inventory = inventoryRepository.findBySku(sku)
+                .orElseThrow(()-> new InventoryNotFoundException("Inventory is not found for SKU:" + sku + "in warehouse"));
+        return InventoryResponse.fromEntity(inventory);
+
+    }
+    public List<InventoryResponse>getAllInventory(){
+        return inventoryRepository.findAll().stream()
+                .map(InventoryResponse::fromEntity)
+                .collect(Collectors.toList());
+    }
+
+    //
+    public List<InventoryResponse> getInventoryByWarehouse(String warehouseId){
+        return inventoryRepository.findByWarehouseId(warehouseId).stream()
+                .map(InventoryResponse::fromEntity)
+                .collect(Collectors.toList());
+    }
+
+    public List<InventoryResponse> getLowStockItems(){
+        return inventoryRepository.findLowStocksItems().stream()
+                .map(InventoryResponse::fromEntity)
+                .collect(Collectors.toList());
+    }
+    @Transactional
+    @CacheEvict(value = "inventory" ,key = "#sku")
+    public InventoryResponse updateQuantity(String sku, Integer quantity){
+        Inventory inventory = inventoryRepository.findBySkuWithLock(sku)
+                .orElseThrow(()-> new InventoryNotFoundException("Inventory not found for SKU: " + sku));
+
+
+        inventory.setQuantity(quantity);
+        Inventory updated = inventoryRepository.save(inventory);
+        log.info("Updated quantity for SKU: {} to {}",updated.getSku(),quantity);
+        KafkaTemplate.send("inventory-updated",sku,updated);
+
+        return InventoryResponse.fromEntity(updated);
+    }
+    @Transactional
+    public boolean checkAvailability(String sku,Integer quantity){
+        Inventory inventory = inventoryRepository.findBySku(sku)
+                .orElseThrow(()-> new InventoryNotFoundException("Inventory not found for sku "+sku));
+        return inventory.getAvailableQuantity()>=quantity;
+    }
+
+    @Transactional
+    @CacheEvict(value = "inventory", key = "#sku")
+    public void reserveStock(String sku, Integer quantity){
+        Inventory inventory = inventoryRepository.findBySkuWithLock(sku)
+                .orElseThrow(()->new InventoryNotFoundException("Inventory not found for SKU: "+ sku));
+
+        if(inventory.getAvailableQuantity() < quantity){
+            throw new InsufficientStockException("Insufficient stock for sku" + sku);
+
+        }
+        inventory.setReservedQuantity(inventory.getReservedQuantity() + quantity);
+        inventoryRepository.save(inventory);
+
+        log.info("Reserved {} units for SKU: {}",quantity,sku);
+        KafkaTemplate.send("inventory-reserved",sku,quantity);
+    }
+
+    public void releaseReservedStock(String sku,Integer quantity){
+        Inventory inventory = inventoryRepository.findBySkuWithLock(sku)
+                .orElseThrow(()->new InventoryNotFoundException("Inventory not found sku : " + sku));
+
+        inventory.setReservedQuantity(inventory.getReservedQuantity() - quantity);
+        inventory.setQuantity(inventory.getQuantity() - quantity);
+        inventoryRepository.save(inventory);
+
+        log.info("Released {} units for SKU: {}",quantity,sku);
+        KafkaTemplate.send("inventory-released",sku,quantity);
+    }
+
+
+}
