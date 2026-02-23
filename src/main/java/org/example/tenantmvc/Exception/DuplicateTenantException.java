f0a5bf66e0483d428383077d9c6493a4e2d5f947
SUMIT <rais74184@gmail.com>
2026-02-23 10:17:39 +0530

    dockerised-redis-remain

--- src/main/java/org/example/tenantmvc/Exception/DuplicateTenantException.java
+++ src/main/java/org/example/tenantmvc/Exception/DuplicateTenantException.java
@@ -1,13 +1,12 @@
 package org.example.tenantmvc.Exception;
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-
-@AllArgsConstructor
-@NoArgsConstructor
-
 public class DuplicateTenantException extends RuntimeException {
-    public DuplicateTenantException(String message){
+
+    public DuplicateTenantException(String message) {
         super(message);
+    }
+
+    public DuplicateTenantException(String message, Throwable cause) {
+        super(message, cause);
     }
 }
--- src/main/java/org/example/tenantmvc/TenantMvcApplication.java
+++ src/main/java/org/example/tenantmvc/TenantMvcApplication.java
@@ -2,8 +2,12 @@ package org.example.tenantmvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+//import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+//@EnableEurekaServer
+@EnableCaching
 public class TenantMvcApplication {
 
     public static void main(String[] args) {
